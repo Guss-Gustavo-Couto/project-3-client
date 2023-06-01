28f5bab779732d11abef9fad367fc16edb736212
Gustavo Couto <gustavo.emanuel.couto@gmail.com>
2023-06-01 17:08:33 +0100

    merge

--- src/pages/ComunityPage/ComunityPage.jsx
+++ src/pages/ComunityPage/ComunityPage.jsx
@@ -1,8 +1,26 @@
-function ComunityPage() {
-  return (
-    <h1>Comunity Page</h1>
-    /* MAP display all users */
-  )
-}
+import { useState, useEffect } from "react";
 
-export default ComunityPage;
+function ComunityPage(props) {
+  const [users] = useState([]);
+
+  useEffect(()=>{
+    setGallerys(props.users);
+  }, [props.users]);;
+ 
+    return (
+      <div>
+          <h2>Comunnity Page</h2>
+          {users.map((user)=>{
+              return(
+                  <div>
+                      <h3>{user.name}</h3>
+                      <p>{user.description}</p>
+                      <p>{user.description}</p>
+                  </div>
+              )
+          })}
+      </div>
+    )
+  }
+
+  export default ComunityPage;
--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -1,12 +1,38 @@
-function DetailsPage() {
-  return <div>
-    <h1>DetailsPage</h1>;
+import {useParams, Link} from "react-router-dom";
+import {useState, useEffect} from 'react';
 
+function GalleryDetailsPage(props) {
+ 
+  const [gallery, setGallery] = useState("");
+  const {galleryId} = useParams();
+
+  const {gallerys} = props;
+
+  useEffect(()=>{
+        const foundGallery = gallerys.find((oneGallery)=>{
+        return oneGallery._id === galleryId;
+    })
+
+    setGallery(foundGallery);
+
+  }, [gallerys])
+
+  return (
+    <div>
+        {   
+            gallery && (
+            <div>
+                <h2>{gallery.name}</h2>
+                <h3>Tech Stack: {gallery.technologies}</h3>
+                <p>{gallery.description}</p>
+                <Link to="/gallerys">Back</Link>
+            </div>
+        )}
     </div>
-
+  )
 }
 
-export default DetailsPage;
+export default GalleryDetailsPage;
 
 
 
